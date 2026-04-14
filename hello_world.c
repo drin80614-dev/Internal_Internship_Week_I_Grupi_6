01c5afbba7f1c901b28ada84df3df59b67e104fd
elisa2468 <ez246@bgt.school>
2026-04-14 11:57:30 +0200

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+git checkout -b Elisa-Zogaj
+mkdir Day1
+cd Day1
+
+
+touch hello_world.c
+
+gcc hello_world.c -o hello_world
+./hello_world
+
+git add .
+git commit -m "Day 1 - Hello World program"
+git push origin Elisa-Zogaj
