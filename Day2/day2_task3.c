9b5adeddc84f7eb1d43bb2347a5188d236b23f50
drin80614-dev <dk244@bgt.school>
2026-04-15 13:58:19 +0200

    Add files via upload

---
+++ Day2/day2_task3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1:
+            printf("Monday - Workday\n");
+            break;
+        case 2:
+            printf("Tuesday - Workday\n");
+            break;
+        case 3:
+            printf("Wednesday - Workday\n");
+            break;
+        case 4:
+            printf("Thursday - Workday\n");
+            break;
+        case 5:
+            printf("Friday - Workday\n");
+            break;
+        case 6:
+            printf("Saturday - Weekend\n");
+            break;
+        case 7:
+            printf("Sunday - Weekend\n");
+            break;
+        default:
+            printf("Invalid input\n");
+    }
+
+    return 0;
+}
