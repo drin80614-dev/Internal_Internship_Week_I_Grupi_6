c807d16cdc4aa87aeeaf30682c6c2150472d801c
EgliShaljani <es245@bgt.school>
2026-04-15 14:40:11 +0200

    Create Day2_task4.c

---
+++ Day 2/Day2_task4.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int mosha;
+    char iniciali;
+    double pikeParaprake;
+    int opsioni;
+
+    printf("Shkruaj moshen: ");
+    scanf("%d", &mosha);
+
+    printf("Shkruaj inicialin: ");
+    scanf(" %c", &iniciali);
+
+    printf("Shkruaj piket paraprake: ");
+    scanf("%lf", &pikeParaprake);
+
+    printf("Zgjedh punetorine (1, 2 ose 3): ");
+    scanf("%d", &opsioni);
+
+    printf("\n--- Permbledhja e Regjistrimit ---\n");
+    printf("Mosha: %d\n", mosha);
+    printf("Iniciali: %c\n", iniciali);
+    printf("Piket paraprake: %.2lf\n", pikeParaprake);
+
+    if (mosha >= 14 && mosha <= 18) {
+        printf("Pjesemarrja sipas moshes: Lejohet\n");
+    }
+    else {
+        printf("Pjesemarrja sipas moshes: Nuk lejohet\n");
+    }
+
+    if (pikeParaprake < 50) {
+        printf("Vleresimi: Nevojitet me shume ushtrim\n");
+    }
+    else if (pikeParaprake < 80) {
+        printf("Vleresimi: Gati per vazhdim\n");
+    }
+    else {
+        printf("Vleresimi: Gati per sfide\n");
+    }
+
+    switch (opsioni) {
+        case 1:
+            printf("Punetoria e zgjedhur: Programim Bazik\n");
+            break;
+        case 2:
+            printf("Punetoria e zgjedhur: Algoritme\n");
+            break;
+        case 3:
+            printf("Punetoria e zgjedhur: Zgjidhje Problemesh\n");
+            break;
+        default:
+            printf("Punetoria e zgjedhur: Opsion i pavlefshem\n");
+            break;
+    }
+
+    return 0;
+}
